ffe2d35b48675fa24b2e5f6b6a69f7f2f558119c
Mathieu Pillard <mpillard@mozilla.com>
2015-01-20 17:29:10 +0100

    Disable cache in travis builds, it's actually slower
    (Stolen from mozilla/olympia#427)

--- scripts/travis_es.sh
+++ scripts/travis_es.sh
@@ -4,7 +4,8 @@ TARGET="/tmp/elasticsearch"
 
 if [ ! -f "$TARGET/elasticsearch-1.3.7/bin/elasticsearch" ]; then
     echo "$TARGET not found.  Building..."
-    pushd $TARGET
+    mkdir -p $TARGET
+    cd $TARGET
     wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.3.7.tar.gz
     tar xvf elasticsearch-1.3.7.tar.gz
     elasticsearch-1.3.7/bin/plugin -install elasticsearch/elasticsearch-analysis-icu/2.3.0
